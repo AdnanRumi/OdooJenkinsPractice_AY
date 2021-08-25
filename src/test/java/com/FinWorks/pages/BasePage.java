60b4a6cb06802f949fd5fa99fea9991500d6c886
AdnanRumi <mugterib@gmail.com>
2021-08-25 09:02:46 +0200

    Commit BasePage

---
+++ src/test/java/com/FinWorks/pages/BasePage.java
@@ -0,0 +1,91 @@
+package com.FinWorks.pages;
+
+import com.FinWorks.utilities.BrowserUtils;
+import com.FinWorks.utilities.Driver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.CacheLookup;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.List;
+
+public abstract class BasePage {
+
+
+
+    @FindBy(xpath = "/html/body/div[1]/div[2]/div[1]/ol/li']")
+    public WebElement pageSubTitle;
+
+    @FindBy(css = "[class= o_loading]")
+    @CacheLookup
+    protected WebElement loaderMask;
+
+    @FindBy(xpath = "//span[@class='oe_topbar_name']")
+    public WebElement userName;
+
+    @FindBy(linkText = "Log out")
+    public WebElement logOutLink;
+
+    @FindBy(linkText = "o_user_menu open")
+    public WebElement myUser;
+
+
+
+    public BasePage() {
+        PageFactory.initElements(Driver.get(), this);
+    }
+
+
+    /**
+     * @return page name, for example: Dashboard
+     */
+    public String getPageSubTitle() {
+        //ant time we are verifying page name, or page subtitle, loader mask appears
+        waitUntilLoaderScreenDisappear();
+//        BrowserUtils.waitForStaleElement(pageSubTitle);
+        return pageSubTitle.getText();
+    }
+
+
+    /**
+     * Waits until loader screen present. If loader screen will not pop up at all,
+     * NoSuchElementException will be handled  bu try/catch block
+     * Thus, we can continue in any case.
+     */
+    public void waitUntilLoaderScreenDisappear() {
+        try {
+            WebDriverWait wait = new WebDriverWait(Driver.get(), 5);
+            wait.until(ExpectedConditions.invisibilityOf(loaderMask));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public String getUserName(){
+        waitUntilLoaderScreenDisappear();
+        BrowserUtils.waitForVisibility(userName, 5);
+        return userName.getText();
+    }
+
+
+
+    public void logOut(){
+        BrowserUtils.waitFor(2);
+        BrowserUtils.clickWithJS(userName);
+        BrowserUtils.clickWithJS(logOutLink);
+    }
+    public void goToMyUser(){
+        waitUntilLoaderScreenDisappear();
+        BrowserUtils.waitForClickablility(userName, 5).click();
+        BrowserUtils.waitForClickablility(myUser, 5).click();
+
+    }
+
+
+
+}
