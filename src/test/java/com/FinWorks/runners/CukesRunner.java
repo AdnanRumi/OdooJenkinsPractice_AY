9264267367e5078d3edd4a73559387ed1d817d7a
AdnanRumi <mugterib@gmail.com>
2021-08-25 09:04:25 +0200

    Commit CukesRunner

---
+++ src/test/java/com/FinWorks/runners/CukesRunner.java
@@ -0,0 +1,21 @@
+package com.FinWorks.runners;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        plugin = {"json:target/cucumber.json",
+                "html:target/default-html-reports",
+                "rerun:target/rerun.txt"
+        },
+        features = "src/test/resources/features",
+        glue = "com/FinWorks/step_definitions",
+        dryRun = false,
+        tags = "@basicSteps"
+)
+
+public class CukesRunner {
+
+}
